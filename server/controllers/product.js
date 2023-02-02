8dd3e982806efa6bc5e93684b5d7b0404c4821e9
Devansh Ashar <ashar.devansh@gmail.com>
2023-02-03 00:10:35 +0530

    product routes added

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,17 +3,76 @@ const app=express()
 const Product=require('../models/product.model')
 app.use(express.json())
 const newProduct=async(req,res)=>{
-    const {prodName, brand, model, price, category, specs,seller,sellerEmail,rating}=req.body;
-    if(!prodId || !prodName || !brand || !model|| !price || !category|| !specs || !seller || !sellerEmail || !quantity)
-    return res.status(400).json({error:"Please fill the necessary details "})
-    const sellerData=await User.findOne({email:sellerEmail})
-    if(!sellerData)
-    return res.status(400).json({error:'Invalid credentials'})
+    const {prodName, brand,userPrice,algoPrice, category,url,yearsUsed}=req.body;
+    if(!prodName || !brand || !userPrice || !category || !url || !yearsUsed)
+    return res.status(400).json({message:"Please fill the necessary details "})
     const prod=new Product(req.body) 
     try {
         await prod.save()
+        await Product.findByIdAndUpdate(prod._id,{sellerEmail:userData.email})
+        await Product.findByIdAndUpdate(prod._id,{seller_id:userData._id})
         res.json({message:'Success'}).status(200)
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(400).json({message:error.message})
     }
 }
+const updateProd=async(req,res)=>{
+    try {
+        let data=await Product.findByIdAndUpdate(req.params.id,req.body)
+        if(userData.email!=data.sellerEmail)
+        return res.status(400).json({message:'Unauthorized'})
+        res.status(200).json({message:'Updated'})
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+const myProducts=async(req,res)=>{
+    try{
+        let data=await Product.find({seller_id:userData._id})
+        res.status(400).json({data})
+    }catch(error){
+        res.status(400).json({message:error.message})
+    }
+}
+const deleteProd=async(req,res)=>{
+    try {
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(200).json({message:'Product Deleted'})
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+const products=async(req,res)=>{
+    try {
+        const data=await Product.find()
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+const searchedProduct=async(req,res)=>{
+    try {
+        const search=req.body
+        const data1=await Product.find({prodName:search})
+        const data2=await Product.find({brand:search})
+        const data3=await Product.find({category:search})
+        res.status(200).json({data1,data2,data3})
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+const particularProduct=async(req,res)=>{
+    try {
+        const prod=await Product.findById(req.params.id)
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
+}
+const interested=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+module.exports={newProduct,updateProd,deleteProd,myProducts,products,searchedProduct,particularProduct}
---
+++ server/routes/product.route.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const router=new express.Router()
+const {
+    newProduct,
+    updateProd,
+    deleteProd,
+    myProducts,
+    products,
+    searchedProduct,
+    particularProduct
+}=require('../controllers/product')
+const authenticate=require('../middlewares/auth.middleware')
+router.post('/newProduct',authenticate,newProduct)
+router.patch('/:id',authenticate,updateProd)
+router.delete('/:id',authenticate,deleteProd)
+router.get('/myProducts',authenticate,myProducts)
+router.get('/',authenticate,products)
+router.post('/search',authenticate,searchedProduct)
+router.get('/:id',authenticate,particularProduct)
+module.exports=router
