a2dcc27433b4f2357063e87aa2cce9b33a751d5b
aman <aman2003nambisan@gmail.com>
2023-02-03 03:40:34 +0530

    home page auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { Login } from "./pages/Login/Login";
 import { Signup } from "./pages/Signup/Signup";
 import { ForgotPassword } from "./pages/ForgotPassword/ForgotPassword";
 import { NewPassword } from "./pages/NewPassword/NewPassword";
-import AboutUs from "./pages/AboutUs/AboutUs";
+import {AboutUs} from "./pages/AboutUs/AboutUs";
 import { Home } from "./pages/Home/Home";
 import { CreateListing } from "./pages/CreateListing/CreateListing";
 import { useApp } from "./context/app-context";
@@ -13,39 +13,22 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export function App() {
-    const location = useLocation();
-    const navigate = useNavigate();
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    // const { currentUser, setCurrentUser } = useApp();
 
-    return (
-        <Routes>
-            {userData === {} ? (
-                <>
-                    <Route path="/" element={<Landing />}></Route>
-                    <Route path="/about-us" element={<AboutUs />}></Route>
-                    <Route path="/login" element={<Login />}></Route>
-                    <Route path="/signup" element={<Signup />}></Route>
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgotPassword />}
-                    ></Route>
-                    <Route
-                        path="/new-password"
-                        element={<NewPassword />}
-                    ></Route>
-                </>
-            ) : (
-                <>
-                    <Route path="/home" element={<Home />}></Route>
-                    <Route path="/about-us" element={<AboutUs />}></Route>
-                    <Route
-                        path="/create-listing"
-                        element={<CreateListing />}
-                    ></Route>
-                </>
-            )}
-            <Route path="*" element={<Error404Page />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />}></Route>
+      <Route path="/about-us" element={<AboutUs />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+      <Route path="/new-password" element={<NewPassword />}></Route>
+
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/create-listing" element={<CreateListing />}></Route>
+      <Route path="*" element={<Error404Page />} />
+    </Routes>
+  );
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,3 +1,54 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useState } from 'react';
+
 export function Home() {
-    return <div>Home</div>;
+
+    const navigate = useNavigate()
+    const [userData, setUserData] = useState({})
+
+    useEffect(() => {
+      getHomePage()
+    }, [])
+    
+    const getHomePage = async () =>{
+        try{
+            const res = await fetch("/AdminPage", {
+                method: "GET",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                credentials: "include"
+              }) 
+              if (res.status !== 200) {
+                throw new Error
+              }
+              const data = await res.json()
+              setUserData(data)
+        } catch(err) {
+            navigate("/")
+            console.log(err)
+        }
+    }
+
+    const buyPageRouting = () =>{
+        navigate("")
+    }
+
+    const sellPageRouting = () =>{
+        navigate("")
+    }
+
+    return (
+        <>
+            <div className="contentForHomePage">
+                <button className="bigButton" onClick={buyPageRouting}>
+                    I want to buy
+                </button>
+                <button className="bigButton" onClick={sellPageRouting}>
+                    I want to sell
+                </button>
+            </div>
+        </>
+    )
 }
